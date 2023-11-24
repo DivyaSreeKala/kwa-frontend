19a79a026f58100488c3270b089e125ac5f2de35
DivyaSreeKala <divyasreekala99@gmail.com>
2023-11-24 20:51:25 +0530

    second commit

--- src/components/ComplaintEdit.js
+++ src/components/ComplaintEdit.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Complaint.css'
 import '../App.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { FaFilePdf } from "react-icons/fa6";
+import  Axios  from 'axios';
 function ComplaintEdit() {
   const navigate=useNavigate()
+  const {id}=useParams() 
+  const [values,setValues]=useState({})
+  useEffect(()=>{
+    Axios.get("http://localhost:3002/api/v2/getComplaintById/"+id,{
+
+  })
+  .then((res)=>{
+    setValues(res.data.complaint)
+    
+    console.log(res.data)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+  },[])
   const UpdateContent=()=>{
       alert("updated")
   }
@@ -11,23 +28,58 @@ function ComplaintEdit() {
       navigate('/complaint-details')
   }
   return (
-    <div className='modal-container' >
-      <div className='modal'>
+    <div className='outer-box' >
+      <div className='registration'>
       <h3 style={{color:"blue"}}>Edit Complaint</h3>
       <form action='#'>
         <div className='form-inside'>
-        First Name<input type='text'/><br/>
-        Last Name<input type='text'/><br/>
-        Address<textarea/><br/>
-        Complaint<input type='text'/><br/>
-        Consumer ID<input type='text'/><br/>
-        Consumer No.<input type='text'/><br/>
-        Mobile Number<input type='text'/><br/>
-        Attachment<input type='file'/><br/>
-        Current Progress<input type='text'/><br/>
-        Remarks<input type='text'/><br/>
+        <div className='form-inside' >
+          <div className="input-field">
+        <label>Consumer ID</label>
+        <input className='type-1' type='text' value={values.consumer_id}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Consumer No.</label>
+        <input className='type-1' type='text' value={values.consumer_number}/><br/>
+        </div>
+        <div className="input-field">
+        <label>First Name</label>
+        <input className='type-1' type='text' value={values.first_name}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Last Name</label>
+        <input className='type-1' type='text' value={values.last_name}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Address</label>
+        <textarea className='address' value={values.address}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Mobile Number</label>
+        <input className='type-1' type='text' value={values.mobile_number}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Complaint</label>
+        <input className='type-1' type='text' value={values.complaint}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Attachment</label>
+        <input className='file-select' type='file' />
+        <a href='#' target='_blank'><FaFilePdf  style={{width:'1.6rem',height:'1.6rem'}} /></a><br/>
+        </div>
+        <div className="input-field">
+        <label>Current Status</label>
+        <input className='type-1' type='text' value={values.current_progress}/><br/>
+        </div>
+        <div className="input-field">
+        <label>Remarks</label>
+        <input className='type-1' type='text' value={values.remarks}/><br/>
+        </div>
+        </div>
+        <div className='save-close'>
         <button className='calculate' onClick={UpdateContent}>Save</button>
         <button style={{backgroundColor:'green'}} className='calculate' onClick={Close}>Close</button>
+        </div>
         </div>
       </form>
       </div>
--- src/components/ComplaintView.js
+++ src/components/ComplaintView.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import { FaFilePdf } from "react-icons/fa6";
 import Axios from 'axios'
 function ComplaintView() {
     const navigate=useNavigate()
     const {id}=useParams()
     const [values,setValues]=useState({})
-    console.log(id)
+    
     useEffect(()=>{
       Axios.get("http://localhost:3002/api/v2/getComplaintById/"+id,{
 
@@ -13,7 +14,7 @@ function ComplaintView() {
     .then((res)=>{
       setValues(res.data.complaint)
       
-      console.log(values)
+      console.log(res.data)
     })
     .catch((error)=>{
       console.log(error)
@@ -28,44 +29,56 @@ function ComplaintView() {
       <h3 style={{color:"blue"}}>View Complaint Details</h3>
       
       <div className="data-field">
+      <div className='label-1'><label >Complaint ID:</label></div>
+        <label className='label-2'>{values.id}</label>
+        </div>
+      <div className="data-field">
       <div className='label-1'><label >Consumer ID:</label></div>
-        <label className='label-2'>{values.first_name}</label>
+        <label className='label-2'>{values.consumer_id}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Consumer No.:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.consumer_number}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>First Name:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.first_name}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Last Name:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.last_name}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Address:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.address}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Mobile Number:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.mobile_number}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Complaint:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.complaint}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Attachment:</label></div>
-        <label className='label-2'>hi</label>
+        <a href={values.attachment_path} target='_blank'><FaFilePdf  style={{width:'1.6rem',height:'1.6rem'}} /></a>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Current Status:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.current_progress}</label>
         </div>
         <div className="data-field">
         <div className='label-1'><label>Remarks:</label></div>
-        <label className='label-2'>hi</label>
+        <label className='label-2'>{values.remarks}</label>
+        </div>
+        <div className="data-field">
+        <div className='label-1'><label>Created At:</label></div>
+        <label className='label-2'>{values.createdAt}</label>
+        </div>
+        <div className="data-field">
+        <div className='label-1'><label>Updated At:</label></div>
+        <label className='label-2'>{values.updatedAt}</label>
         </div>
         <button style={{backgroundColor:'green'}} className='calculate' onClick={View}>Close</button>
         
